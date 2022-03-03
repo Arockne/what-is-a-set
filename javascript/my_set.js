c6150eb2a792f49fc5f203bb4497c6844266d547
Daniel Calvin <arockne2003@yahoo.com>
2022-03-03 12:49:01 -0700

    commit tree bb8579703001f168d6a496eaca00d901c0971ef5

--- javascript/my_set.js
+++ javascript/my_set.js
@@ -3,44 +3,64 @@ class MySet {
   // if an iterable is provided only its unique values should be in data
   // strings and arrays will need to be broken down by their elements/characters
   constructor(iterable) {
+    if (!(Array.isArray(iterable) 
+      || typeof iterable === 'string'
+      || iterable === undefined
+    )) {
+      throw new Error('Must pass in a string, array, or nothing')
+    }
+
     this.data = {};
+
+    if (iterable) {
+      for (const value of iterable) {
+        this.data[value] = true;
+      }
+    }
   }
 
   // return number of elements in MySet
   size() {
-
+    return Object.keys(this.data).length
   }
 
   // add an item to MySet as is
   // don't worry about arrays here!
   // return the MySet instance
   add(item) {
-
+    this.data[item] = true
+    return this
   }
 
   // delete an item from MySet
   // don't worry about arrays here!
   // return true if successful, otherwise false
   delete(item) {
-
+    if (this.has(item)) {
+      delete this.data[item]
+      return true
+    }
+    return false
   }
 
   // return true if in MySet, otherwise false
   // don't worry about arrays here!
   has(item) {
-
+    return !!this.data[item]
   }
 
   // return data as an array
   // don't worry about arrays here!
   entries() {
-
+    return Object.keys(this.data)
   }
 }
 
 if (require.main === module) {
   // add your own tests in here
-  
+  const unique = new MySet([1, 2, 3, 3])
+  console.log(unique)
+  console.log(unique.entries())
 }
 
 module.exports = MySet;
